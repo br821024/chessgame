250fdf79d4099bf155ec89b7cc4cbbc360f64fae
br821024 <br821024@yahoo.com.tw>
2016-04-17 11:29:35 +0800

    Added files via upload

--- github/src/Client/Center.java
+++ github/src/Client/Center.java
@@ -2,6 +2,7 @@ package Client;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -11,61 +12,114 @@ public class Center extends JFrame {
 
 	ClientThread controller = null;
 	
+	ArrayList<roomButton> roomlist = new ArrayList<roomButton>(); 
+	
 	JButton B_Create = new basicButton("創建房間",850,40,150,30);
 	JButton B_Profile = new basicButton("個人資訊",850,80,150,30);
 	JButton B_Leave = new basicButton("離開大廳",850,120,150,30);
 	JButton B_Exit = new basicButton("離開遊戲",850,160,150,30);
 	
 	public Center(ClientThread controller){
-		B_Create.addActionListener(new CenterListener(controller));
-		B_Profile.addActionListener(new CenterListener(controller,"Profile"));
-		B_Leave.addActionListener(new CenterListener(controller,"Logout"));
-		B_Exit.addActionListener(new CenterListener(controller,"End"));
+		B_Create.addActionListener(new RoomListener(controller,"Create"));
+		B_Profile.addActionListener(new RoomListener(controller,"Profile"));
+		B_Leave.addActionListener(new RoomListener(controller,"Logout"));
+		B_Exit.addActionListener(new RoomListener(controller,"End"));
 		addWindowListener(new CloseListener(controller));
 		this.controller = controller;
 		setSize(1030,796);
 		setLayout(null);
 		setVisible(false);
-		add(B_Create);	//創建房間
-		add(B_Profile);	//個人資訊
-		add(B_Leave);	//離開大廳
-		add(B_Exit);	//離開遊戲
+		add(B_Create);
+		add(B_Profile);
+		add(B_Leave);
+		add(B_Exit);
+	}
+	
+	public void updateRoom(){
+		int x = 0; int y = 0;
+		for(int i=0;i<roomlist.size();i++){
+			roomButton target = roomlist.get(i);
+			if(target.isFull){
+				target.setBounds(20,620+40*(y),500,30);
+				y++;
+			}
+			else{
+				target.setBounds(20+250*(x%3),20+40*(x/3),230,30);
+				x++;
+			}
+		}
+	}
+	public void addRoom(roomButton room){
+		roomlist.add(room);
+		this.add(room);
+		updateRoom();
+	}
+	public void joinRoom(String account,String player,int side){
+		roomButton target = getRoom(account);
+		target.isFull = true;
+		target.setplayer(player,side);
+		updateRoom();
+	}
+	public void leaveRoom(String account,String player,int side){
+		roomButton target = getRoom(account);
+		target.isFull = false;
+		target.setplayer("",side);
+		updateRoom();
+	}
+	public roomButton getRoom(String account){
+		roomButton room = null;
+		for(int i=0;i<roomlist.size();i++){
+			if(roomlist.get(i).account.equals(account)) room = roomlist.get(i);
+		}
+		return room;
+	}
+	public void deleteRoom(String account){
+		for(int i=0;i<roomlist.size();i++){
+			if(roomlist.get(i).account.equals(account)) roomlist.remove(i);
+		}
 	}
 }
 
-class CenterListener implements ActionListener{
+class RoomListener implements ActionListener{
 	
 	ClientThread controller = null;
-	Center center = null;
+	String account = null;
 	String action = null;
-	int type = -1;
+
 	int side = -1;
 	
-	public CenterListener(ClientThread controller,String action) {
+	public RoomListener(ClientThread controller,String action,String account,int side){
+		this.controller = controller;
+		this.account = account;
+		this.action = action;
+		this.side = side;
+	}
+	public RoomListener(ClientThread controller,String action) {
 		this.controller = controller;
 		this.action = action;
 	}
-	
-	public CenterListener(ClientThread controller) {
-		this.controller = controller;
-	}
-	
+
 	public void actionPerformed(ActionEvent arg0) {
 		if(action!=null){
-			controller.send(action+"|");
-		}
-		else{
-			new setDialog(this);
+			if(action.equals("Create")){
+				new setDialog(this);
+			}
+			else if(action.equals("Join")){
+				if(side==2) new setDialog(this);
+				else controller.send(action+"|"+account+"|"+(1-side));
+			}
+			else controller.send(action);
 		}
 	}
 	
-	public void createRoom(int side){
-		controller.send("Create|"+side);
+	public void actionPerformed(ActionEvent arg0,int side){
+		if(action.equals("Join")) controller.send(action+"|"+account+"|"+side);
+		else controller.send(action+"|"+side);
 	}
 }
 
 class setDialog extends JDialog {
-	public setDialog(CenterListener center) {
+	public setDialog(RoomListener center) {
 		setTitle("Setting");
 		setSize(146,180);
 		setLocation(125,165);
@@ -77,7 +131,7 @@ class setDialog extends JDialog {
 	}
 	
 	class setButton extends JButton {
-		public setButton(CenterListener center,setDialog dialog,int side) { // Side = 0 White = 1 Black = 2 Don't mind
+		public setButton(RoomListener listener,setDialog dialog,int side) { // Side = 0 White = 1 Black = 2 Don't mind
 			switch(side){
 				case 0:
 					setText("白棋");
@@ -91,25 +145,25 @@ class setDialog extends JDialog {
 				default:
 					setText("Error");
 			}
-			addActionListener(new setListener(center,dialog,side));
+			addActionListener(new setListener(listener,dialog,side));
 			setBounds(15,15+side*40,100,30);
 		}
 	}
 	
 	class setListener implements ActionListener{
 		
-		CenterListener center = null;
-		setDialog dialog = null;
+		RoomListener listener = null;
+		JDialog dialog = null;
 		int side = -1;
 		
-		public setListener(CenterListener center,setDialog dialog,int side) {
-			this.center = center;
+		public setListener(RoomListener listener,JDialog dialog,int side) {
+			this.listener = listener;
 			this.dialog = dialog;
 			this.side = side;
 		}
 		
 		public void actionPerformed(ActionEvent arg0) {
-			center.createRoom(side);
+			listener.actionPerformed(arg0,side);
 			dialog.dispose();
 		}
 	}
@@ -120,13 +174,17 @@ class roomButton extends JButton {
 	private String[] Symbol = {"\u25cb","\u25cf","\u25d1"}; // \u25cb \u25cf \u2658 \u265e
 	private String Text = null;
 	
-	public String[] player = {"","",""};
-	public final int formatlength = 32;
+	private final int formatlength = 32;
 	
-	public roomButton(ClientThread controller,String account,int index,String player,int side){ // Side = 0 White = 1 Black = 2 Don't mind
-		addActionListener(new RoomListener(account,controller));
+	public String account = null;
+	public String[] player = {"","",""};
+	
+	public boolean isFull = false;
+	
+	public roomButton(ClientThread controller,String account,String player,int side){ // Side = 0 White = 1 Black = 2 Don't mind
+		addActionListener(new RoomListener(controller,"Join",account,side));
 		setFont(new Font("Arial Unicode MS", Font.PLAIN, 16));
-		setBounds(index,index,230,30);
+		this.account = account;
 		setplayer(player,side);
 	}
 	
@@ -134,17 +192,14 @@ class roomButton extends JButton {
 		this.player[side] = player;
 		formatText(side);
 	}
-	
 	private void formatText(int side){
-		if(player[0].equals("") || player[1].equals("")){
+		if(!isFull){
 			Text = Symbol[side]+" %1$"+format(player[side],1)+"s"+player[side]+"%2$"+format(player[side],0)+"s";
 			Text = String.format(Text,"","");
-			setSize(230,30);
 		}
 		else{
 			Text = "%1$"+format(player[0],1)+"s"+Symbol[0]+" "+player[0]+"%2$"+format(player[0],0)+"s"+"　v.s　"+" %1$"+format(player[1],0)+"s"+Symbol[1]+" "+player[1]+"%2$-"+format(player[1],1)+"s";
 			Text = String.format(Text,"","","","");
-			setSize(500,30);
 		}
 		setText(Text);
 	}
@@ -156,20 +211,5 @@ class roomButton extends JButton {
 			return (formatlength-name.length())/2+1;
 		else
 			return (formatlength-name.length())/2;
-	}
-}
-
-class RoomListener implements ActionListener {
-	
-	ClientThread controller;
-	String account;
-	
-	public RoomListener(String account,ClientThread controller){
-		this.controller = controller;
-		this.account = account;
-	}
-
-	public void actionPerformed(ActionEvent arg0) {
-		controller.send("Join|"+account);
 	}
 }
--- github/src/Client/GUI.java
+++ github/src/Client/GUI.java
@@ -1,5 +1,7 @@
 package Client;
 
+import java.util.StringTokenizer;
+
 import javax.swing.JFrame;
 
 public class GUI extends JFrame {
@@ -12,20 +14,48 @@ public class GUI extends JFrame {
 		controller = client;
 		login = new Login(controller);
 		center = new Center(controller);
+		//center.setVisible(true);
+		//center.addRoom(new roomButton(controller,"Aneros","Aneros",0));
+		//center.addRoom(new roomButton(controller,"Anoymous","Anoymous",0));
+		//center.addRoom(new roomButton(controller,"Anoymous1","Anoymous1",0));
+		//center.addRoom(new roomButton(controller,"Anoymous2","Anoymous2",2));
+		//center.addRoom(new roomButton(controller,"Anoymous3","Anoymous3",2));
+		//center.addRoom(new roomButton(controller,"Anoymous4","Anoymous4",2));
+		//center.joinRoom("Aneros","join1",1);
 	}
 	
+	public void Room(String action,StringTokenizer token){
+		if(action.equals("Create")){ center.addRoom(new roomButton(controller,token.nextToken(),token.nextToken(),Integer.parseInt(token.nextToken()))); }
+		else if(action.equals("Join")){ center.joinRoom(token.nextToken(),token.nextToken(),Integer.parseInt(token.nextToken())); }
+		else if(action.equals("Room")){
+			String account = token.nextToken();
+			String host = token.nextToken();
+			roomButton room = null;
+			int side = Integer.parseInt(token.nextToken());
+			room = new roomButton(controller,account,host,side);
+			center.addRoom(room);
+			if(token.hasMoreTokens()) center.joinRoom(account,token.nextToken(),1-side);
+		}
+	}
 	public void Login(){
 		login.setVisible(false);
 		center.setVisible(true);
 	}
-	
 	public void Logout(){
 		login.setVisible(true);
 		center.setVisible(false);;
 	}
-	
+	public void Play(int Side){
+		center.setVisible(false);
+		login.setVisible(true);
+	}
+	public void Wait(){
+		center.setVisible(false);
+		login.setVisible(true);
+	}
 	public void End(){
 		login.dispose();
 		center.dispose();
 	}
+	
 }
